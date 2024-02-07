3b0020503b88e106cb09464cbeadd4e3ddda9288
Alex Eickelman <57731927+Aeickelman40@users.noreply.github.com>
2024-02-06 19:46:47 -0800

    Add constants, middleware, initial error handling

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,41 +1,59 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Get all contacts
 // @route GET /api/contacts
 // @access public
 
-const getContacts = (req, res) => {
-    res.status(200).json({ message: "Get all contacts" })
-}
+const getContacts = asyncHandler(
+    async (req, res) => {
+        res.status(200).json({ message: "Get all contacts" })
+    }
+)
 
 // @desc Get unique contact
 // @route GET /api/contacts/:id
 // @access public
 
-const getContact = (req, res) => {
-    res.status(200).json({ message: `Get contact for ${req.params.id}` })
-}
+const getContact = asyncHandler(
+    async (req, res) => {
+        res.status(200).json({ message: `Get contact for ${req.params.id}` })
+    }
+)
 
 // @desc Create New contact
 // @route POST /api/contacts
 // @access public
 
-const createContact = (req, res) => {
-    res.status(201).json({ message: "Create Contact" })
-}
+const createContact = asyncHandler(
+    async (req, res) => {
+        console.log("This request body is:", req.body)
+        const { name, email, phone } = req.body
+        if (!name || !email || !phone) {
+            res.status(400)
+            throw new Error("All fields are mandatory!")
+        }
+        res.status(201).json({ message: "Create Contact" })
+    }
+)
 
 // @desc Update contact
-// @route PUT /api/contacts
+// @route PUT /api/contacts/:id
 // @access public
 
-const updateContact = (req, res) => {
-    res.status(200).json({ message: `Update contact for ${req.params.id}` })
-}
+const updateContact = asyncHandler(
+    async (req, res) => {
+        res.status(200).json({ message: `Update contact for ${req.params.id}` })
+    }
+)
 
 // @desc Delete contact
-// @route DELETE /api/contacts
+// @route DELETE /api/contacts/:id
 // @access public
 
-const deleteContact = (req, res) => {
-    res.status(200).json({ message: `Delete contact for ${req.params.id}` })
-}
+const deleteContact = asyncHandler(
+    async (req, res) => {
+        res.status(200).json({ message: `Delete contact for ${req.params.id}` })
+    }
+)
 
 module.exports = { getContacts, createContact, getContact, updateContact, deleteContact }
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
 
+const errorHandler = require('./middleware/errorHandler');
+
 const app = express();
 
 const port = process.env.port || 8000
 
+app.use(express.json())
+
 app.use('/api/contacts', require('./routes/contactRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
