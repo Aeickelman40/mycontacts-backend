4d09494771cfe031618fcf314d67b58dd7a21885
Alex Eickelman <57731927+Aeickelman40@users.noreply.github.com>
2024-02-06 22:01:35 -0800

    Refine user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcrypt')
+
+const User = require("../models/userModal")
 
 // @desc Register a user
 // @route POST /api/users/register
@@ -6,6 +9,37 @@ const asyncHandler = require('express-async-handler')
 
 const registerUser = asyncHandler(
     async (req, res) => {
+
+        const { userName, email, password } = req.body
+
+        if (!userName || !email || !password) {
+            res.status(400)
+            throw new Error("All fields are mandatory!")
+        }
+
+        const userAvailable = await User.findOne({ email });
+        if (userAvailable) {
+            res.status(400);
+            throw new Error("User already registered!")
+        }
+
+        // Hash password
+        const hashedPassword = await bcrypt.hash(password, 10)
+        console.log('hashed pw', hashedPassword)
+        const user = await User.create({
+            userName,
+            email,
+            password: hashedPassword
+        })
+
+        console.log('user', user)
+        if (user) {
+            res.status(201).json({ _id: user.id, email: user.email })
+        } else {
+            res.status(400)
+            throw new Error("User data is not valid")
+        }
+
         res.json({ message: "Register User" })
     }
 )
